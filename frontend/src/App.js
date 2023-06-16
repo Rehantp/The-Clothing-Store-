2e46755c29fc4b6f039a53484e83917f7a8e2fa0
Rehantp <rehamc123@gmail.com>
2023-06-16 17:14:04 +0530

    init v.0.0.0

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">The Clothing Store</a>
+      </header>
+      <main>list Products</main>
+    </div>
+  );
+}
+
+export default App;

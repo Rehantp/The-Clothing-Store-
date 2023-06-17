9e23698f13ba0901c138a16107bc9b8bc791c2d6
Rehantp <rehamc123@gmail.com>
2023-06-17 09:19:21 +0530

    products init v0.0.0

---
+++ frontend/src/data.js
@@ -0,0 +1,46 @@
+// data class
+const data = {
+  products: [
+    {
+      name: "Nike Tshirt",
+      slug: "Nike-Tshirt",
+      category: "Shirts",
+      image: "/images/p1.png",
+      price: 6000,
+      conuntInstock: 10,
+      brand: "Nike",
+      rating: 4.8,
+      numReviews: 10,
+      description:
+        "Our floral print Tour Polo is all about consistency, with a stretchy, sweat-wicking feel that helps you get the most out your swing every time.",
+    },
+    {
+      name: "Puma Tshirt",
+      slug: "Puma-Tshirt",
+      category: "Shirts",
+      image: "/images/p3.png",
+      price: 6200,
+      conuntInstock: 10,
+      brand: "Puma",
+      rating: 4.8,
+      numReviews: 10,
+      description:
+        "A modern take on timeless motorsport style, the latest addition to PUMA's partnership with BMW Motorsport features pops of color and a tonal graphic pattern. This classic jacquard polo is accented by a stand-up ribbed collar with contrast piping and co-branded details.",
+    },
+    {
+      name: "Puma Polo",
+      slug: "Puma-Polo",
+      category: "Shirts",
+      image: "/images/p4.png",
+      price: 6200,
+      conuntInstock: 10,
+      brand: "Puma",
+      rating: 4.8,
+      numReviews: 10,
+      description:
+        "This polo is a study in understated style, but don't let its simplicity fool you. Where else can casual meet minimalist and still look this good? We'll wait.",
+    },
+  ],
+};
+
+export default data;

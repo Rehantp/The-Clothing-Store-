a331967c816675c597ed63984fd202bf13d43d4a
Rehantp <rehamc123@gmail.com>
2023-06-27 22:50:38 +0530

    bk fetch v2

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,6 @@ app.get("/api/products/slug/:slug", (req, res) => {
   } else {
     res.status(404).send({ messeage: "Product not found" });
   }
-  res.send(data.products);
 });
 
 const port = process.env.PORT || 5000;

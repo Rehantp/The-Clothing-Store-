2a21c87828cb5a124d19fce1dbf120a23c2aae18
Rehantp <rehamc123@gmail.com>
2023-07-17 09:40:25 +0530

    bk server

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import Product from "../models/productModel.js";
-
+import data from "../data.js";
 const seedRouter = express.Router();
 
 seedRouter.get("/", async (req, res) => {
-  await Product.remove({});
+  await Product.deleteOne({});
   const createdProducts = await Product.insertMany(data.products);
   res.send({ createdProducts });
 });
